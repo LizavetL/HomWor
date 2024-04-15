bdea394d8e513199dbaa88be4bc498a373848971
Liza <lizaveta14-01@mail.ru>
2024-04-15 15:48:11 +0700

    Первая задача

---
+++ hw1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 7 == 0)
+{
+    if (num % 23 == 0)
+    {
+        Console.WriteLine("да");
+    }
+else
+{
+    Console.WriteLine("нет");
+}
+}
+else
+{
+    Console.WriteLine("нет");
+}

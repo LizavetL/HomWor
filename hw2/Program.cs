71b97ea4b7c52c04cc4923bc9ce46aaa5a5876ce
Liza <lizaveta14-01@mail.ru>
2024-04-15 15:48:22 +0700

    Вторая задача

---
+++ hw2/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите координаты х");
+int x = int.Parse(Console.ReadLine()) ;
+
+Console.WriteLine("Введите координаты у");
+int y = int.Parse(Console.ReadLine()) ;
+
+if (x == 0)
+{
+    Console.WriteLine("координаты x не должны равнться 0"); 
+}
+if (y == 0)
+{
+    Console.WriteLine("координаты y не должны равнться 0"); 
+}
+
+if (x > 0)
+{
+    if (y > 0)
+    Console.WriteLine("I"); 
+}
+if (x > 0)
+{
+    if (y < 0)
+    Console.WriteLine("II"); 
+}
+if (x < 0)
+{
+    if (y < 0)
+    Console.WriteLine("III"); 
+}
+if (x < 0)
+{
+    if (y > 0)
+    Console.WriteLine("IV"); 
+}

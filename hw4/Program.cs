2ff2d8bb4be62fc923ad6abc065e70fdd6408815
Liza <lizaveta14-01@mail.ru>
2024-04-15 15:48:45 +0700

    Четвертая задача

---
+++ hw4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число");
+
+string num = Console.ReadLine();
+string mun = "" ;
+int line = 0 ;
+
+for (int i = num.Length-1; i >= 0; i--) 
+{
+    mun += num ;
+}
+
+int mun0 = Convert.ToInt32(mun) ;
+while (mun0 % 10 > 0 ) // должно быть другое ограниченье
+{
+    line = mun0 % 10 ;
+    Console.Write(line+ ", ");
+    mun0 = mun0 / 10 ;
+}

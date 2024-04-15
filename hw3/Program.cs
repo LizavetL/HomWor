4e1a21b05165fb5f170f0772312e0cc1ca0b7e25
Liza <lizaveta14-01@mail.ru>
2024-04-15 15:48:32 +0700

    Третья задача

---
+++ hw3/Program.cs
@@ -0,0 +1,13 @@
+﻿int num = new Random().Next(10,100);
+
+int fnum = num / 10 ;
+int snum = num % 10;
+
+if (fnum > snum)
+{
+    Console.WriteLine(fnum);
+}
+else
+{
+    Console.WriteLine(snum);
+}

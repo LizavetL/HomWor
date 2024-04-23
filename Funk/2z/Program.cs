1a536590b4e1e28cb668a3acdc1a792ed7941f7f
Liza <lizaveta14-01@mail.ru>
2024-04-23 16:49:16 +0700

    Вторая задача

---
+++ Funk/2z/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] array =new int[5];
+int eveN = 0;
+
+void FillingАrray()
+{
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+}
+}
+
+int CountingEvenElements()
+{
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        eveN++;
+    }
+}
+return eveN;
+}
+
+
+void OutputValue()
+{
+Console.Write(eveN);
+}
+
+FillingАrray();
+CountingEvenElements();
+OutputValue();
